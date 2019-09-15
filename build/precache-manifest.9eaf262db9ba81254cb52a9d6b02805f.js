580d17ef0866487c10dc1db6871819c75cac1303
ArmWeb <armen_aslikian@mail.ru>
2019-09-15 17:36:11 +0400

    made changes for gh-pages

---
+++ build/precache-manifest.9eaf262db9ba81254cb52a9d6b02805f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f66fa84f067253aa68d57312c242fd9",
+    "url": "/type-racer/index.html"
+  },
+  {
+    "revision": "3eb6dc955b77fff3730f",
+    "url": "/type-racer/static/css/main.8f61a5c6.chunk.css"
+  },
+  {
+    "revision": "2930b6c28f151d17b931",
+    "url": "/type-racer/static/js/2.7daca264.chunk.js"
+  },
+  {
+    "revision": "3eb6dc955b77fff3730f",
+    "url": "/type-racer/static/js/main.bd207a49.chunk.js"
+  },
+  {
+    "revision": "44b7da38003c2193a08b",
+    "url": "/type-racer/static/js/runtime~main.bdbb1d63.js"
+  }
+]);
